1e3e1e2fdb1858924717796ca5b42ab3c60a78c7
Craig <cragt369@gmail.com>
2022-09-29 18:17:06 -0500

    Finish game when score = 5, add reset button

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Roshambo
+
 The classic Rock Paper Scissors game
 
 This is a project made in JavaScript, meant to be played in the console. I plan on adding a UI using HTML/CSS in the future.
--- script.js
+++ script.js
@@ -1,57 +1,116 @@
 let computerChoice;
 let randomNumber;
-// const playerSelection = 'rock';
-// const computerSelection = getComputerChoice();
+let playerSelection;
+let computerSelection;
+let playerScore = 0;
+let computerScore = 0;
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+const reset = document.getElementById("reset");
 
 function getComputerChoice() {
-    randomNumber = Math.floor((Math.random() * 3) + 1);
-    if (randomNumber === 1) {
-        computerChoice = 'rock';
-    } else if (randomNumber === 2){
-        computerChoice = 'paper';
-    } else {
-        computerChoice = 'scissors';
-    }
-    return computerChoice;
+  randomNumber = Math.floor(Math.random() * 3 + 1);
+  if (randomNumber === 1) {
+    computerChoice = "rock";
+  } else if (randomNumber === 2) {
+    computerChoice = "paper";
+  } else {
+    computerChoice = "scissors";
+  }
+  return computerChoice;
 }
 
 function playRound(playerSelection, computerSelection) {
-// Player chooses rock
-if (playerSelection === 'rock' && computerSelection === 'rock') {
+  // Player chooses rock
+  if (playerSelection === "rock" && computerSelection === "rock") {
     return `It's a draw!`;
-} else if (playerSelection === 'rock' && computerSelection === 'paper') {
-    return `You lose! Paper beats rock!`
-} else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-    return `You win! Rock beats scissors!`
-// Player chooses paper
-} else if (playerSelection === 'paper' && computerSelection === 'rock') {
-    return `You win! Paper beats rock!`
-} else if (playerSelection === 'paper' && computerSelection === 'paper') {
-    return `It's a draw!`
-} else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-    return `You lose! Scissors beats paper!`
-// Player chooses scissors
-} else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-    return `You lose! Rock beats scissors!`
-} else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-    return `You win! Scissors beats rock!`
-} else if (playerSelection === 'scissors' && computerSelection === 'scissors') {
-    return `It's a draw!`
-} else {
-    return `Please enter rock, paper, or scissors, idiot`
-}
+  } else if (playerSelection === "rock" && computerSelection === "paper") {
+    computerScore++;
+    return `You lose! Paper beats rock!`;
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    playerScore++;
+    return `You win! Rock beats scissors!`;
+    // Player chooses paper
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    playerScore++;
+    return `You win! Paper beats rock!`;
+  } else if (playerSelection === "paper" && computerSelection === "paper") {
+    return `It's a draw!`;
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    computerScore++;
+    return `You lose! Scissors beats paper!`;
+    // Player chooses scissors
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    computerScore++;
+    return `You lose! Rock beats scissors!`;
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    playerScore++;
+    return `You win! Scissors beats rock!`;
+  } else if (
+    playerSelection === "scissors" &&
+    computerSelection === "scissors"
+  ) {
+    return `It's a draw!`;
+  } else {
+    return `Please enter rock, paper, or scissors, idiot`;
+  }
 }
 function game() {
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = prompt('Choose rock, paper, or scissors').toLowerCase();
-        const computerSelection = getComputerChoice();
-        console.log(`Player chooses ${playerSelection}, Computer chooses ${computerSelection}`)
-        console.log(playRound(playerSelection, computerSelection));
-    }
+  const computerSelection = getComputerChoice();
+  document.getElementById("results").innerHTML =
+    "<h3>" +
+    `Player chooses ${playerSelection}, Computer chooses ${computerSelection}` +
+    "<br>" +
+    playRound(playerSelection, computerSelection) +
+    "</h3>";
+  document.getElementById("score").innerHTML =
+    "<h1> Player Score: " +
+    playerScore +
+    " Computer Score: " +
+    computerScore +
+    "</h1>";
+  if (playerScore === 5) {
+    document.getElementById("winner").innerHTML =
+      "<h1>Congratulation! You won!</h1>";
+    playerScore = 0;
+    computerScore = 0;
+    rock.removeEventListener("click", runEvent);
+    paper.removeEventListener("click", runEvent);
+    scissors.removeEventListener("click", runEvent);
+  } else if (computerScore === 5) {
+    document.getElementById("winner").innerHTML = "<h1>Awww, you lost!</h1>";
+    playerScore = 0;
+    computerScore = 0;
+    rock.removeEventListener("click", runEvent);
+    paper.removeEventListener("click", runEvent);
+    scissors.removeEventListener("click", runEvent);
+  }
 }
 
-game();
+rock.addEventListener("click", runEvent);
+paper.addEventListener("click", runEvent);
+scissors.addEventListener("click", runEvent);
+reset.addEventListener("click", resetGame);
 
-// test
+function runEvent(e) {
+  if (e.target.id === "rock") {
+    playerSelection = "rock";
+  } else if (e.target.id === "paper") {
+    playerSelection = "paper";
+  } else if (e.target.id === "scissors") {
+    playerSelection = "scissors";
+  }
+  game();
+}
 
-
+function resetGame() {
+  playerScore = 0;
+  computerScore = 0;
+  document.getElementById("results").innerHTML = "";
+  document.getElementById("score").innerHTML = "";
+  document.getElementById("winner").innerHTML = "";
+  rock.addEventListener("click", runEvent);
+  paper.addEventListener("click", runEvent);
+  scissors.addEventListener("click", runEvent);
+}
